af6306070929b09c0a538ac7244db4541c387655
taemyung <tmheo74@gmail.com>
2015-10-24 18:53:39 +0900

    initial import

---
+++ src/main/java/tmheo/MyDynamoApplication.java
@@ -0,0 +1,12 @@
+package tmheo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDynamoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyDynamoApplication.class, args);
+    }
+}
